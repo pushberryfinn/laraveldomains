f01143e7335a7fa46d6433862fa246d3590471e7
Atdhe Krasniqi <atniqii@gmail.com>
2026-05-10 05:13:05 +0200

    fix: rewrite domain:make-migration to not extend MigrateMakeCommand to avoid hijacking make:migration

--- src/Console/MakeDomainMigrationCommand.php
+++ src/Console/MakeDomainMigrationCommand.php
@@ -2,24 +2,86 @@
 
 namespace Pushberryfinn\LaravelDomains\Console;
 
-use Illuminate\Database\Console\Migrations\MigrateMakeCommand;
+use Illuminate\Console\Command;
+use Illuminate\Database\Migrations\MigrationCreator;
+use Illuminate\Support\Composer;
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-class MakeDomainMigrationCommand extends MigrateMakeCommand
+class MakeDomainMigrationCommand extends Command
 {
     protected $name = 'domain:make-migration';
+    protected $description = 'Create a new migration file inside a domain';
 
-    protected function getMigrationPath()
-    {
-        $domain = $this->input->getOption('domain');
-        return app_path("Domains/{$domain}/database/migrations");
+    public function __construct(
+        protected MigrationCreator $creator,
+        protected Composer $composer
+    ) {
+        parent::__construct();
     }
 
-    protected function getOptions()
+    public function handle(): void
     {
-        return array_merge(parent::getOptions(), [
+        $domain = $this->option('domain');
+
+        if (!$domain) {
+            $this->components->error('The --domain option is required.');
+            return;
+        }
+
+        $name   = Str::snake(trim($this->argument('name')));
+        $table  = $this->option('table');
+        $create = $this->option('create') ?: false;
+
+        if (!$table && is_string($create)) {
+            $table  = $create;
+            $create = true;
+        }
+
+        if (!$table) {
+            [$table, $create] = $this->guessTableName($name);
+        }
+
+        $path = app_path("Domains/{$domain}/database/migrations");
+
+        if (!is_dir($path)) {
+            mkdir($path, 0755, true);
+        }
+
+        $file = $this->creator->create($name, $path, $table, $create);
+
+        $this->components->info(sprintf('Migration [%s] created successfully.', $file));
+    }
+
+    protected function guessTableName(string $name): array
+    {
+        if (preg_match('/^create_(\w+)_table$/', $name, $matches)) {
+            return [$matches[1], true];
+        }
+
+        foreach (['_to_', '_from_', '_in_'] as $token) {
+            if (str_contains($name, $token)) {
+                return [Str::after($name, $token), false];
+            }
+        }
+
+        return [null, false];
+    }
+
+    protected function getArguments(): array
+    {
+        return [
+            ['name', InputArgument::REQUIRED, 'The name of the migration'],
+        ];
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            ['create', null, InputOption::VALUE_OPTIONAL, 'The table to be created'],
+            ['table',  null, InputOption::VALUE_OPTIONAL, 'The table to migrate'],
             ['domain', null, InputOption::VALUE_REQUIRED, 'The domain name'],
-        ]);
+        ];
     }
 }
